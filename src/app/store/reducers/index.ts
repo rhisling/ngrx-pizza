47635bc5397731f0c01842e8929220e3a5eeca17
Aravindhan Elayakumar <aravindhansai@gmail.com>
2019-12-08 21:23:52 -0800

    Adding the router state tree

--- src/app/store/reducers/index.ts
+++ src/app/store/reducers/index.ts
@@ -1,5 +1,10 @@
 import * as fromRouter from '@ngrx/router-store';
-import { Params } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Params,
+} from '@angular/router';
+import { ActionReducerMap, createFeatureSelector } from '@ngrx/store';
 
 export interface RouterStateUrl {
   url: string;
@@ -11,6 +16,25 @@ export interface State {
   routerReducer: fromRouter.RouterReducerState<RouterStateUrl>;
 }
 
-export const reducers = {
+export const reducers: ActionReducerMap<State> = {
   routerReducer: fromRouter.routerReducer,
 };
+
+export const getRouterState = createFeatureSelector<
+  fromRouter.RouterReducerState
+>('routerReducer');
+
+// custom serializer
+export class CustomSerializer
+  implements fromRouter.RouterStateSerializer<RouterStateUrl> {
+  serialize(routerState: RouterStateSnapshot): RouterStateUrl {
+    const { url } = routerState;
+    const { queryParams } = routerState.root;
+    let state: ActivatedRouteSnapshot = routerState.root;
+    while (state.firstChild) {
+      state = state.firstChild;
+    }
+    const { params } = state;
+    return { url, queryParams, params };
+  }
+}
